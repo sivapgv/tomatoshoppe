6305a5c20afbe0dceba1fed4680d6d58ea541938
sivapgv <sivapgv@gmail.com>
2022-12-17 16:55:26 +0530

    Add api server

---
+++ api-server/routes/customerlist.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const customerlist = require('../services/customerlist');
+
+/* GET customer. */
+router.get('/', async function(req, res, next) {
+    try {
+        res.json(await customerlist.getCustomerList(req.query.page));
+    } catch (err) {
+        console.error(`Error while getting customer `, err.message);
+        next(err);
+    }
+});
+
+/* Post customer. */
+router.post('/', async function(req, res, next) {
+    try {
+        res.json(await customerlist.createcustomer(req.body));
+    } catch (err) {
+        console.error(`Error while creating customer`, err.message);
+        next(err);
+    }
+});
+
+module.exports = router;
---
+++ api-server/services/customerlist.js
@@ -0,0 +1,39 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function getCustomerList(page = 1){
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    `SELECT id, customername, customertype, address 
+    FROM customers`
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data
+  }
+}
+
+async function createcustomer(customer){
+    const result = await db.query(
+      `INSERT INTO customers 
+      (customername, customertype, address, outletarea, phonenumber, tinnumber) 
+      VALUES 
+      ('${customer.customername}', '${customer.customertype}', '${customer.address}', '${customer.outletarea}', '${customer.phonenumber}', '${customer.tinnumber}')`
+    );
+  
+    let message = "customer error";
+  
+    if (result.affectedRows) {
+      message = 'Customer created successfully';
+    }
+  
+    return {message};
+}
+
+module.exports = {
+    getCustomerList,
+    createcustomer
+}
